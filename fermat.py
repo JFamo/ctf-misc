3db5b0b0719d9ef9ffca1a4a0c53d6e6e177c5e4
J. Famous <joshua.famous@gmail.com>
2019-10-10 03:58:02 -0400

    Import RsaTool

---
+++ fermat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import gmpy2
+
+def fermat_factor(n):
+    assert n % 2 != 0
+
+    a = gmpy2.isqrt(n)
+    b2 = gmpy2.square(a) - n
+
+    while not gmpy2.is_square(b2):
+        a += 1
+        b2 = gmpy2.square(a) - n
+
+    p = a + gmpy2.isqrt(b2)
+    q = a - gmpy2.isqrt(b2)
+
+    return int(p), int(q)
